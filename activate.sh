644520a32bbbdd6402a6a20fbc10078f4841a408
peter <peterghost86@gmail.com>
2022-02-19 16:01:09 +0800

    [activate] Windows support

--- activate.sh
+++ activate.sh
@@ -2,4 +2,13 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-export PYTHONPATH=${PYTHONPATH}:${DIR}:${DIR}/examples
+if [[ "$OSTYPE" == "msys" ]]; then
+    DIR="$(cygpath -d $DIR)"
+    SEP=";"
+    DELIM="\\"
+else
+    SEP=":"
+    DELIM="/"
+fi
+
+export PYTHONPATH=${PYTHONPATH}${SEP}${DIR}${SEP}${DIR}${DELIM}examples
