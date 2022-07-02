50fa5ef8a7b83ac2771fe75a81b15ecf376f8ee3
peterjc123 <peterghost86@gmail.com>
2022-07-02 10:16:33 +0800

    [tests] refactor modifier_torchvision_test

--- tests/modifier_torchvision_test.py
+++ tests/modifier_torchvision_test.py
@@ -5,19 +5,16 @@ import torchvision.models
 import inspect
 import logging
 import os
+import re
 import unittest
 
 import numpy as np
 
 from tinynn.graph.tracer import patch_helper, trace, tracer_context, model_tracer
-import models
-
 from tinynn.prune.oneshot_pruner import OneShotChannelPruner
+from tinynn.util.util import import_from
 
-
-def import_from(module, name):
-    module = __import__(module, fromlist=[name])
-    return getattr(module, name)
+from common_utils import collect_torchvision_models, prepare_inputs
 
 
 def transform_output(output):
@@ -30,14 +27,18 @@ def transform_output(output):
     return new_output
 
 
-BLACKLIST = ()
+BLACKLIST = (
+    'convnext.*',
+    'vit.*',
+    'swin.*'
+)
 
 
 class TestModelMeta(type):
     @classmethod
     def __prepare__(mcls, name, bases):
         d = dict()
-        test_classes = mcls.collect_torchvision_models()
+        test_classes = collect_torchvision_models()
         for test_class in test_classes:
             test_name = f'test_torchvision_model_{test_class.__name__}'
             d[test_name] = mcls.build_model_test(test_class)
@@ -49,28 +50,16 @@ class TestModelMeta(type):
         def f(self):
             model_name = model_class.__name__
 
-            if model_name in BLACKLIST:
-                raise unittest.SkipTest('IN BLACKLIST')
+            for item in BLACKLIST:
+                if re.match(item, model_name):
+                    raise unittest.SkipTest('IN BLACKLIST')
 
             with model_tracer():
                 m = model_class()
                 m.eval()
                 print(f'\n---------prune {model_name} ing!---------\n')
-                if hasattr(m, 'custom_input_shape'):
-                    input_shape = m.custom_input_shape
-                elif model_name == 'inception_v3':
-                    input_shape = (1, 3, 299, 299)
-                else:
-                    input_shape = (1, 3, 224, 224)
 
-                if type(input_shape[0]) not in (tuple, list):
-                    input_shape = (input_shape,)
-
-                inputs = []
-                for shape in input_shape:
-                    t = torch.ones(shape)
-                    inputs.append(t)
-
+                inputs = prepare_inputs(m)
                 outputs = m(*inputs)
                 outputs = transform_output(outputs)
 
@@ -80,25 +69,6 @@ class TestModelMeta(type):
                 outputs = m(*inputs)
 
         return f
-
-    @classmethod
-    def collect_torchvision_models(cls):
-        torchvision_model_classes = []
-        for key in torchvision.models.__dict__:
-            item = getattr(torchvision.models, key)
-            if inspect.isfunction(item):
-                no_arg = True
-                has_pretrained = False
-                for p in inspect.signature(item).parameters.values():
-                    if p.name != 'kwargs' and p.default is p.empty:
-                        no_arg = False
-                        break
-                    elif p.name == 'pretrained':
-                        has_pretrained = True
-                if no_arg and has_pretrained:
-                    torchvision_model_classes.append(item)
-
-        return torchvision_model_classes
 
 
 class TestModel(unittest.TestCase, metaclass=TestModelMeta):
