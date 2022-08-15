727b44b99373b9adaad9e4962814308eea30a0d3
peter <peterghost86@gmail.com>
2022-08-15 20:47:04 +0800

    [quantizer] fix quantization for SiLU

--- tests/qat_module_test.py
+++ tests/qat_module_test.py
@@ -2,7 +2,7 @@ import unittest
 
 import torch
 import torch.nn as nn
-from tinynn.graph.quantization.modules import QPReLU
+from tinynn.graph.quantization.modules import QPReLU, QSiLU
 
 
 class QATModuleTester(unittest.TestCase):
@@ -34,6 +34,26 @@ class QATModuleTester(unittest.TestCase):
             quant = QPReLU(orig)
 
             inp = (torch.randn((1, 32, 24, 24)) - 0.5) * 2
+
+            orig_outp = orig(inp)
+            quant_outp = quant(inp)
+
+            if not torch.allclose(orig_outp, quant_outp):
+                print('original:')
+                print(orig_outp)
+                print('quanted:')
+                print(quant_outp)
+
+                print('diff (min, max):', torch.max(quant_outp - orig_outp), torch.min(quant_outp - orig_outp))
+
+                self.assertTrue(False)
+
+    def test_silu(self):
+        for i in range(100):
+            orig = nn.SiLU()
+            quant = QSiLU(orig)
+
+            inp = (torch.randn((100, 100)) - 0.5) * 2
 
             orig_outp = orig(inp)
             quant_outp = quant(inp)
