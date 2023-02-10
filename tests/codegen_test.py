1cbc3b56a4690d64245913323dee1df5cd24949a
peterjc123 <peterghost86@gmail.com>
2023-02-10 16:19:46 +0800

    [tests] improve codegen test

--- tests/codegen_test.py
+++ tests/codegen_test.py
@@ -1,6 +1,11 @@
 import re
 import unittest
 
+import torch  # noqa: F401
+import torch.nn as nn  # noqa: F401
+import torch.nn.functional as F  # noqa: F401
+import torchvision  # noqa: F401
+
 from tinynn.graph.tracer import fetch_modules, gen_module_constrctor_line
 
 
@@ -10,21 +15,35 @@ def collect_testcases():
     for mod in modules:
         name = mod.__name__
         if mod.__doc__ is not None:
+            full_instances = re.findall(
+                rf'>>> .* = (nn.{name}\(.*\))\n *>>> input = (.*)\n *>>> output = .*?\((.*)\)', mod.__doc__
+            )
+            known_instances = set((full_instance[0] for full_instance in full_instances))
             instances = re.findall(rf'nn\.{name}\(.*\)', mod.__doc__)
-            usages.extend(list(set(instances)))
-            if len(instances) == 0:
+            for instance in instances:
+                if instance not in known_instances:
+                    full_instances.append((instance, None, None))
+            usages.extend(list(set(full_instances)))
+            if len(full_instances) == 0:
                 print(f'{name} is skipped (no instances found)')
         else:
             print(f'{name} is skipped (doc missing)')
 
     results = []
-    for usage in usages:
+    for usage, dummy, args in usages:
+        if dummy is not None and args is not None:
+            content = f'm = {usage}\ninput = {dummy}\noutput = m({args})'
+        else:
+            content = f'm = {usage}'
         try:
             m = eval(usage)
-        except Exception:
+            if dummy is not None and args is not None:
+                input = eval(dummy)  # noqa: F841
+                output = eval(f'm({args})')  # noqa: F841
+        except Exception as e:
+            print(f'{usage} is skipped: {content} errored with "{e}"')
             continue
-
-        results.append((usage, m))
+        results.append((usage, dummy, args, m))
     return results
 
 
@@ -34,23 +53,30 @@ class TestModelMeta(type):
         d = dict()
         test_cases = collect_testcases()
         counter = dict()
-        for usage, test_mod in test_cases:
+        for usage, dummy, args, test_mod in test_cases:
             cls = type(test_mod)
             count = counter.get(cls, 0)
             count += 1
             test_name = f'test_{cls.__name__}_{count}'
-            d[test_name] = mcls.build_model_test(usage, test_mod)
+            d[test_name] = mcls.build_model_test(usage, dummy, args, test_mod)
             counter[cls] = count
         return d
 
     @classmethod
-    def build_model_test(cls, usage, test_mod):
+    def build_model_test(cls, usage, dummy, args, test_mod):
         def f(self):
+            if dummy is not None and args is not None:
+                content = f'm = {usage}\ninput = {dummy}\noutput = m({args})'
+            else:
+                content = f'm = {usage}'
             line, _ = gen_module_constrctor_line(test_mod)
             try:
-                eval(line)
+                m = eval(line)  # noqa: F841
+                if dummy is not None and args is not None:
+                    input = eval(dummy)  # noqa: F841
+                    output = eval(f'm({args})')  # noqa: F841
             except Exception:
-                self.fail(f'Cannot restore from {usage}, got {line}')
+                self.fail(f'Cannot restore from {content}, got {line}')
 
         return f
 
