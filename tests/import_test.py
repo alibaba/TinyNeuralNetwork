edb1f75fd34072f6117a2faba71d21341aa0e3e6
zhoukun <zhoukun_1998@qq.com>
2023-03-02 12:20:18 +0800

    [tests] import tests fix

--- tests/import_test.py
+++ tests/import_test.py
@@ -3,46 +3,46 @@ import unittest
 
 class ImportTester(unittest.TestCase):
     def test_oneshot_pruner(self):
-        pass
+        from tinynn.prune.oneshot_pruner import OneShotChannelPruner
 
     def test_admm_pruner(self):
-        pass
+        from tinynn.prune.admm_pruner import ADMMPruner
 
     def test_netadapt_pruner(self):
-        pass
+        from tinynn.prune.netadapt_pruner import NetAdaptPruner
 
     def test_tracer(self):
-        pass
+        from tinynn.graph.tracer import model_tracer, trace, import_patcher
 
     def test_qat(self):
-        pass
+        from tinynn.graph.quantization.quantizer import QATQuantizer
 
     def test_ptq(self):
-        pass
+        from tinynn.graph.quantization.quantizer import PostQuantizer
 
     def test_dyn_q(self):
-        pass
+        from tinynn.graph.quantization.quantizer import DynamicQuantizer
 
     def test_bf16(self):
-        pass
+        from tinynn.graph.quantization.quantizer import BF16Quantizer
 
     def test_converter(self):
-        pass
+        from tinynn.converter import TFLiteConverter
 
     def test_cifar10_util(self):
-        pass
+        from tinynn.util.cifar10 import get_dataloader, train_one_epoch, validate, calibrate
 
     def test_train_util(self):
-        pass
+        from tinynn.util.train_util import DLContext, get_device, train, get_module_device
 
     def test_observer(self):
-        pass
+        from tinynn.graph.quantization.observer import HistogramObserverKL
 
     def test_bn_restore(self):
-        pass
+        from tinynn.util.bn_restore import model_restore_bn
 
     def test_cle(self):
-        pass
+        from tinynn.graph.quantization.algorithm.cross_layer_equalization import cross_layer_equalize
 
 
 if __name__ == '__main__':
