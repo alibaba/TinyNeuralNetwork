9a4258cbb882b487fe671ee4b6596dd7171ac254
zzzzzzzk <41361256+zk1998@users.noreply.github.com>
2023-03-03 16:48:17 +0800

    [util] Add quantization error analysis and weight visualization (#188)
    * [quantization] Add error analysis and weight visualization
    
    * [util] fix training mode bug
    
    * [util]fix code review
    
    * [example] add quantization error usage example
    
    * [example] fix spell error
    
    * [util] fix code review
    
    * [examples] fix typo
    
    * Apply suggestions from code review
    
    Co-authored-by: peterjc123 <peterghost86@gmail.com>
    
    * [examples] fix tupo
    
    ---------
    
    Co-authored-by: peterjc123 <peterghost86@gmail.com>

---
+++ examples/quantization/post_error_anaylsis.py
@@ -0,0 +1,103 @@
+import argparse
+import os
+import sys
+
+CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
+
+sys.path.insert(1, os.path.join(CURRENT_PATH, '../../'))
+
+import torch
+import torch.nn as nn
+
+from examples.models.cifar10.mobilenet import DEFAULT_STATE_DICT, Mobilenet
+from tinynn.graph.quantization.quantizer import QATQuantizer
+from tinynn.graph.tracer import model_tracer
+from tinynn.util.cifar10 import get_dataloader, calibrate
+from tinynn.util.train_util import DLContext, get_device
+from tinynn.graph.quantization.algorithm.cross_layer_equalization import cross_layer_equalize
+from tinynn.graph.quantization.fake_quantize import set_ptq_fake_quantize
+from tinynn.util.quantization_analysis_util import graph_error_analysis, layer_error_analysis, get_weight_dis
+
+
+CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
+
+
+def main_worker(args):
+    with model_tracer():
+        model = Mobilenet()
+        model.load_state_dict(torch.load(DEFAULT_STATE_DICT))
+
+        # Provide a viable input for the model
+        dummy_input = torch.rand((1, 3, 224, 224))
+
+        device = get_device()
+
+        # --------weight visualization usage-------------
+        # Draw the weight distribution for all layers of model.
+        get_weight_dis(model, save_path='out')
+        # -----------------------------------------------
+
+        # When the weight distributions fluctuates greatly, CLE may significantly increase the quantization accuracy.
+        if args.cle:
+            model = cross_layer_equalize(model, dummy_input, device, cle_iters=2)
+
+        # More information for QATQuantizer initialization, see `examples/quantization/qat.py`.
+        # We set 'override_qconfig_func' when initializing QATQuantizer to use fake-quantize to do post quantization.
+        quantizer = QATQuantizer(
+            model, dummy_input, work_dir='out', config={'override_qconfig_func': set_ptq_fake_quantize}
+        )
+        ptq_model = quantizer.quantize()
+
+    print(ptq_model)
+
+    # Use DataParallel to speed up calibrating when possible
+    if torch.cuda.device_count() > 1:
+        ptq_model = nn.DataParallel(ptq_model)
+
+    # Move model to the appropriate device
+    ptq_model.to(device=device)
+
+    context = DLContext()
+    context.device = device
+    context.train_loader, context.val_loader = get_dataloader(args.data_path, 224, args.batch_size, args.workers)
+    context.max_iteration = 100
+
+    # Post quantization calibration
+    ptq_model.apply(torch.quantization.disable_fake_quant)
+    ptq_model.apply(torch.quantization.enable_observer)
+    calibrate(ptq_model, context)
+
+    # Disable observer and enable fake quantization to validate model with quantization error
+    ptq_model.apply(torch.quantization.disable_observer)
+    ptq_model.apply(torch.quantization.enable_fake_quant)
+    dummy_input.to(device)
+    ptq_model(dummy_input)
+    print(ptq_model)
+
+    # Perform quantization error analysis with real dummy input
+    dummy_input_real = next(iter(context.train_loader))[0][:1]
+
+    # --------quantization error analysis usage-------------
+    # The error is accumulated by directly giving the difference in layer output
+    # between the quantized model and the floating model. If you want a quantized model with high accuracy,
+    # the layers closest to the final output should be less than 10%, which means the final
+    # layer's cosine similarity should be greater than 90%.
+    graph_error_analysis(ptq_model, dummy_input_real, metric='cosine')
+
+    # We quantize each layer separately and compare the difference
+    # between the original output and the output with quantization error for each layer,
+    # which is used to calculate the quantization sensitivity of each layer.
+    layer_error_analysis(ptq_model, dummy_input_real, metric='cosine')
+    # -----------------------------------------------
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data-path', metavar='DIR', default="/data/datasets/cifar10", help='path to dataset')
+    parser.add_argument('--config', type=str, default=os.path.join(CURRENT_PATH, 'config.yml'))
+    parser.add_argument('--workers', type=int, default=8)
+    parser.add_argument('--cle', type=bool, default=False)
+    parser.add_argument('--batch-size', type=int, default=128)
+
+    args = parser.parse_args()
+    main_worker(args)
---
+++ tinynn/util/quantization_analysis_util.py
@@ -0,0 +1,323 @@
+import os
+from matplotlib import pyplot as plt
+from typing import List
+
+import torch
+import torch.nn as nn
+
+from torch.nn.parallel.data_parallel import DataParallel
+from torch.nn.parallel.distributed import DistributedDataParallel
+
+from .train_util import get_logger, get_module_device
+
+log = get_logger(__name__, 'INFO')
+
+
+def sqnr(x: torch.Tensor, y: torch.Tensor):
+    Ps = torch.norm(x)
+    Pn = torch.norm(x - y)
+    return (20 * torch.log10(Ps / Pn)).item()
+
+
+def cosine(x: torch.Tensor, y: torch.Tensor, reduction: str = 'mean') -> torch.Tensor:
+    """calulate the cosine similarity between x and y"""
+    if x.shape != y.shape:
+        raise ValueError(f'Can not compute loss for tensors with different shape. ({x.shape} and {y.shape})')
+    reduction = str(reduction).lower()
+
+    if x.ndim == 1:
+        x = x.unsqueeze(0)
+        y = y.unsqueeze(0)
+
+    x = x.flatten(start_dim=1).float()
+    y = y.flatten(start_dim=1).float()
+
+    cosine_sim = torch.cosine_similarity(x, y, dim=-1)
+
+    if reduction == 'mean':
+        return torch.mean(cosine_sim)
+    elif reduction == 'sum':
+        return torch.sum(cosine_sim)
+    elif reduction == 'none':
+        return cosine_sim
+    else:
+        raise ValueError(f'Cosine similarity do not supported {reduction} method.')
+
+
+METRIC_DICT = {
+    'cosine': cosine,
+    'sqnr': sqnr,
+}
+
+
+def error_print(metric, q_errors_activ, q_errors_weight, sort_num):
+    logs = []
+    if len(q_errors_weight) > 0:
+        logs.append('')
+        logs.append(f'Weights ({metric} sorted {sort_num}):')
+        for n, m, e in q_errors_weight:
+            logs.append(f'{n:40} {metric}: {e:.4f}, scale: {m.scale.item():.4f}, zero_point: {m.zero_point.item()}')
+    if len(q_errors_activ) > 0:
+        logs.append('')
+        logs.append(f'Activations ({metric} sorted {sort_num}):')
+        for n, m, e in q_errors_activ:
+            logs.append(f'{n:50} {metric}: {e:.4f}, scale: {m.scale.item():.4f}, zero_point: {m.zero_point.item()}')
+    if len(q_errors_weight) == 0 and len(q_errors_activ) == 0:
+        logs.append('')
+        logs.append('All good!')
+    if len(logs) > 0:
+        logs.insert(0, 'Quantization error report:')
+        logs.append('')
+
+        full_log = '\n'.join(logs)
+        log.warning(full_log)
+
+
+def layer_error_analysis(q_model: nn.Module, dummy_input, metric: str = 'cosine', sort_num: float = 20):
+    """Generates the layerwise quant error report using the given metric, the q_model need to be qat_prepared.
+
+    Args:
+        q_model: The quant prepared model
+        dummy_input: A viable input to the model
+        metric: Metrics for measuring the error of floating point tensor and quantized tensor.
+                Default to be 'cosine', optional 'sqnr'.
+        sort_num : The smallest sort_num layer0 on given metric. Defaults to 20
+    """
+
+    if isinstance(q_model, DataParallel) or isinstance(q_model, DistributedDataParallel):
+        model = q_model.module
+    else:
+        model = q_model
+    metric_fn = METRIC_DICT[metric]
+
+    train_flag = model.training
+    model.eval()
+    with torch.no_grad():
+        modules_list = {}
+        names_list = {}
+        float_results = {}
+        hooks = []
+
+        def forward_hook(module, input, output):
+            name = names_list[module]
+            float_results[name] = input
+
+        fake_quant_enabled_dict = {}
+        observer_enabled_dict = {}
+        for n, m in model.named_modules():
+            if isinstance(m, torch.quantization.FakeQuantize):
+                names_list[m] = n
+                modules_list[n] = m
+
+                fake_quant_enabled_dict[m] = m.fake_quant_enabled.clone()
+                observer_enabled_dict[m] = m.observer_enabled.clone()
+
+                hooks.append(m.register_forward_hook(forward_hook))
+
+        if len(modules_list) == 0:
+            log.warning('No FakeQuantize modules found. Are you sure you had prepared your model?')
+
+        model.apply(torch.quantization.disable_fake_quant)
+        model.apply(torch.quantization.disable_observer)
+
+        device = get_module_device(model)
+
+        if type(dummy_input) == torch.Tensor:
+            actual_input = [dummy_input]
+        elif isinstance(dummy_input, (tuple, list)):
+            actual_input = list(dummy_input)
+        else:
+            log.error(f'Unsupported type {type(dummy_input)} for dummy input')
+            assert False
+
+        for i in range(len(actual_input)):
+            dummy_input = actual_input[i]
+            if type(dummy_input) == torch.Tensor:
+                if dummy_input.device != device:
+                    actual_input[i] = dummy_input.to(device)
+
+        with torch.no_grad():
+            model(*actual_input)
+
+        for h in hooks:
+            h.remove()
+        hooks.clear()
+
+        for m, v in fake_quant_enabled_dict.items():
+            m.fake_quant_enabled = v
+
+        q_errors_weight = []
+        q_errors_activ = []
+        while len(float_results) > 0:
+            n, f = float_results.popitem()
+            mod = modules_list[n]
+            with torch.no_grad():
+                q = mod(*f)
+                loss = metric_fn(f[0], q)
+            actual_n = '.'.join(n.split('.')[:-1])
+            if n.endswith('.weight_fake_quant'):
+                q_errors_weight.append((actual_n, mod, loss))
+            else:
+                q_errors_activ.append((actual_n, mod, loss))
+
+        q_errors_weight = sorted(q_errors_weight, key=lambda x: x[2])
+        q_errors_activ = sorted(q_errors_activ, key=lambda x: x[2])
+
+        q_errors_weight = q_errors_weight[:sort_num]
+        q_errors_activ = q_errors_activ[:sort_num]
+
+        error_print(metric, q_errors_activ, q_errors_weight, sort_num)
+
+        for m, v in observer_enabled_dict.items():
+            m.observer_enabled = v
+
+    if train_flag:
+        model.train()
+
+
+def graph_error_analysis(q_model: nn.Module, dummy_input, metric: str = 'cosine'):
+    """Generates the cumulative quant error report using the given metric, the q_model need to be qat_prepared.
+
+    Args:
+        q_model: The quant prepared model.
+        dummy_input: A viable input to the model
+        metric: Metrics for measuring the error of floating point tensor and quantized tensor.
+                Default to be 'cosine', optional 'sqnr'.
+    """
+    if isinstance(q_model, DataParallel) or isinstance(q_model, DistributedDataParallel):
+        model = q_model.module
+    else:
+        model = q_model
+    metric_fn = METRIC_DICT[metric]
+
+    train_flag = model.training
+    model.eval()
+
+    with torch.no_grad():
+        modules_list = {}
+        names_list = {}
+        results = {}
+        hooks = []
+
+        def forward_hook(module, input, output):
+            name = names_list[module]
+            results[name] = input
+
+        fake_quant_enabled_dict = {}
+        observer_enabled_dict = {}
+        for n, m in model.named_modules():
+            if isinstance(m, torch.quantization.FakeQuantize):
+                names_list[m] = n
+                modules_list[n] = m
+
+                fake_quant_enabled_dict[m] = m.fake_quant_enabled.clone()
+                observer_enabled_dict[m] = m.observer_enabled.clone()
+
+                hooks.append(m.register_forward_hook(forward_hook))
+
+        model.apply(torch.quantization.disable_fake_quant)
+        model.apply(torch.quantization.disable_observer)
+
+        if len(modules_list) == 0:
+            log.warning('No FakeQuantize modules found. Are you sure you had prepared your model?')
+
+        device = get_module_device(model)
+
+        if type(dummy_input) == torch.Tensor:
+            actual_input = [dummy_input]
+        elif isinstance(dummy_input, (tuple, list)):
+            actual_input = list(dummy_input)
+        else:
+            log.error(f'Unsupported type {type(dummy_input)} for dummy input')
+            assert False
+
+        for i in range(len(actual_input)):
+            dummy_input = actual_input[i]
+            if type(dummy_input) == torch.Tensor:
+                if dummy_input.device != device:
+                    actual_input[i] = dummy_input.to(device)
+
+        model(*actual_input)
+
+        # Restore fake-quantize and record activation with quantization error.
+        for m, v in fake_quant_enabled_dict.items():
+            m.fake_quant_enabled = v
+        float_results = results
+        results = {}
+
+        model(*actual_input)
+
+        for h in hooks:
+            h.remove()
+        hooks.clear()
+
+        q_errors_activ = []
+        for name, f_tensor in float_results.items():
+            assert name in results, f'{name} not in results'
+            actual_n = '.'.join(name.split('.')[:-1])
+            loss = metric_fn(f_tensor[0], results[name][0])
+            if not name.endswith('.weight_fake_quant'):
+                q_errors_activ.append((actual_n, modules_list[name], loss))
+
+        error_print(metric, q_errors_activ, [], '')
+
+        for m, v in observer_enabled_dict.items():
+            m.observer_enabled = v
+
+    if train_flag:
+        model.train()
+
+
+def get_weight_dis(
+    model: nn.Module,
+    unique_name_list: List[str] = None,
+    nbins=256,
+    save_path: str = 'out',
+    threshold=20,
+    fig_size=(7, 7),
+):
+    """Draw the weight distribution of model
+
+    Args:
+        model: We recommend use ptq-prepared model to draw fused weight distribution
+        unique_name_list: You can set the layer which you want to get distribution, default to all layer of model
+        nbins: Bins of distribution, default to be 256
+        save_path: Weight distribution fig weill saved at "[save_path]/weight_distribution"
+        threshold: The threshold of weight range to used to prompt anomalies
+        fig_size: Set fig size
+    """
+    with torch.no_grad():
+        save_dir = os.path.join(save_path, 'weight_distribution')
+        log.info(f"jpgs will saved at {save_dir}")
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        warning_layer = dict()
+        for name, mod in model.named_modules():
+            if not hasattr(mod, 'weight'):
+                continue
+            if unique_name_list is None or name in unique_name_list:
+                op_type = type(mod).__name__
+                x = mod.weight.cpu()
+                y = torch.histc(x, nbins)
+                x_min = torch.min(x)
+                x_max = torch.max(x)
+                if x_max - x_min > threshold:
+                    warning_layer[name] = (op_type, float(x_min), float(x_max))
+                bin_width = (x_max - x_min) / nbins
+                x_s = [x_min + (idx + 0.5) * bin_width for idx in range(nbins)]
+
+                fig, ax = plt.subplots(figsize=fig_size)
+                ax.set_yscale('log')
+                ax.plot(x_s, y.detach().numpy())
+                ax.set_title(f'Op_uname:  {name}[{op_type}]')
+                ax.set_xlabel(f'Range:[{x_min:.4f},{x_max:.4f}]')
+                ax.set_ylabel('Count')
+                save_path = os.path.join(save_dir, f'{name}.jpg')
+                plt.savefig(save_path)
+                plt.cla()
+        if warning_layer:
+            log_str = f'\n---------the layer weight range length greater than {threshold}---------\n'
+            for k, v in warning_layer.items():
+                log_str += f'{k}, {v}\n'
+            log_str += '---------------------------------------------------------------'
+            log.warning(log_str)
