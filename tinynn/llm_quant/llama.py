ef221f9b7487b81acf169f82f93baaa0d95d2810
zhoukun <zhoukun_1998@qq.com>
2023-05-31 11:27:30 +0800

    [quantization] init commit for llm quant

---
+++ tinynn/llm_quant/llama.py
@@ -0,0 +1,120 @@
+import math
+from typing import Optional, Tuple
+from distutils.version import LooseVersion
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from transformers.models.llama.modeling_llama import apply_rotary_pos_emb
+from transformers.modeling_utils import set_module_tensor_to_device
+
+
+class LlamaAttentionFused(nn.Module):
+    def __init__(self, origin_attention):
+        super().__init__()
+        self.config = origin_attention.config
+        self.hidden_size = origin_attention.hidden_size
+        self.num_heads = origin_attention.num_heads
+        self.head_dim = origin_attention.head_dim
+        self.max_position_embeddings = origin_attention.max_position_embeddings
+
+        self.qkv_proj = nn.Linear(
+            origin_attention.hidden_size, origin_attention.num_heads * origin_attention.head_dim * 3, bias=False
+        )
+        fused_weight = torch.cat(
+            [
+                fc_node.weight.data
+                for fc_node in [origin_attention.q_proj, origin_attention.k_proj, origin_attention.v_proj]
+            ],
+            dim=0,
+        )
+        set_module_tensor_to_device(
+            self.qkv_proj, 'weight', fused_weight.device, value=fused_weight, dtype=fused_weight.dtype
+        )
+        self.o_proj = origin_attention.o_proj
+        self.rotary_emb = origin_attention.rotary_emb
+
+        origin_attention.q_proj = None
+        origin_attention.k_proj = None
+        origin_attention.v_proj = None
+
+    def _shape(self, tensor: torch.Tensor, seq_len: int, bsz: int):
+        return tensor.view(bsz, seq_len, self.num_heads, self.head_dim).transpose(1, 2).contiguous()
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_value: Optional[Tuple[torch.Tensor]] = None,
+        output_attentions: bool = False,
+        use_cache: bool = False,
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
+        bsz, q_len, _ = hidden_states.size()
+        # use fused fc output to get qkv states
+        qkv_states = self.qkv_proj(hidden_states).view(bsz, q_len, self.num_heads * 3, self.head_dim).transpose(1, 2)
+        (query_states, key_states, value_states) = torch.chunk(qkv_states, 3, 1)
+
+        is_causal = past_key_value is None
+
+        kv_seq_len = key_states.shape[-2]
+        if past_key_value is not None:
+            kv_seq_len += past_key_value[0].shape[-2]
+        cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)
+        query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
+        # [bsz, nh, t, hd]
+
+        if past_key_value is not None:
+            # reuse k, v, self_attention
+            key_states = torch.cat([past_key_value[0], key_states], dim=2)
+            value_states = torch.cat([past_key_value[1], value_states], dim=2)
+
+        past_key_value = (key_states, value_states) if use_cache else None
+        if LooseVersion(torch.__version__) == LooseVersion('1.13.0'):
+            with torch.backends.cuda.sdp_kernel(enable_math=False):
+                attn_output, attn_weights = F._scaled_dot_product_attention(
+                    query_states, key_states, value_states, is_causal=is_causal
+                )
+        elif LooseVersion(torch.__version__) >= LooseVersion('2.0.0'):
+            with torch.backends.cuda.sdp_kernel(enable_math=False):
+                attn_output, attn_weights = F.scaled_dot_product_attention(
+                    query_states, key_states, value_states, is_causal=is_causal
+                )
+        else:
+            attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(self.head_dim)
+
+            if attn_weights.size() != (bsz, self.num_heads, q_len, kv_seq_len):
+                raise ValueError(
+                    f"Attention weights should be of size {(bsz * self.num_heads, q_len, kv_seq_len)}, but is"
+                    f" {attn_weights.size()}"
+                )
+
+            if attention_mask is not None:
+                if attention_mask.size() != (bsz, 1, q_len, kv_seq_len):
+                    raise ValueError(
+                        f"Attention mask should be of size {(bsz, 1, q_len, kv_seq_len)}, but is"
+                        f" {attention_mask.size()}"
+                    )
+                attn_weights = attn_weights + attention_mask
+                attn_weights = torch.max(attn_weights, torch.tensor(torch.finfo(attn_weights.dtype).min))
+
+            # upcast attention to fp32
+            attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
+            attn_output = torch.matmul(attn_weights, value_states)
+
+            if attn_output.size() != (bsz, self.num_heads, q_len, self.head_dim):
+                raise ValueError(
+                    f"`attn_output` should be of size {(bsz, self.num_heads, q_len, self.head_dim)}, but is"
+                    f" {attn_output.size()}"
+                )
+        del query_states, key_states, value_states
+
+        attn_output = attn_output.transpose(1, 2)
+        attn_output = attn_output.reshape(bsz, q_len, self.hidden_size)
+
+        attn_output = self.o_proj(attn_output)
+
+        if not output_attentions:
+            attn_weights = None
+
+        return attn_output, attn_weights, past_key_value
---
+++ tinynn/llm_quant/modules.py
@@ -0,0 +1,230 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import Tensor
+
+from transformers.models.llama.modeling_llama import LlamaAttention
+from tinynn.llm_quant.llama import LlamaAttentionFused
+from tinynn.util.util import get_logger
+
+log = get_logger(__name__, 'INFO')
+SPEEDUP = True
+
+try:
+    from easyquant import (
+        decompress_int4,
+        decompress_int8,
+        quantize_per_token,
+        gemm,
+        dequantize_bias_per_token,
+        dequantize_per_token,
+    )
+except ImportError:
+    log.warning('easyquant is not installed, the inference performance may be degraded')
+    SPEEDUP = False
+
+
+def get_submodule_with_parent_from_name(model, module_name):
+    """Gets the submodule with its parent and sub_name using the name given"""
+    module_name_parts = module_name.split('.')
+    cur_obj = model
+    last_obj = None
+
+    for ns in module_name_parts:
+        last_obj = cur_obj
+        if type(cur_obj) == nn.ModuleList:
+            cur_obj = cur_obj[int(ns)]
+        elif type(cur_obj) == nn.ModuleDict:
+            cur_obj = cur_obj[ns]
+        else:
+            cur_obj = getattr(cur_obj, ns)
+
+    return cur_obj, last_obj, module_name_parts[-1]
+
+
+def compress_int(data_tensor, bit_width, per_channel=True, per_token=False):
+    # use [-127, 127] as 8-bit quant range
+    q_max = 2 ** (bit_width - 1) - 1
+    q_min = -q_max
+
+    assert (per_channel and per_token) is False
+    if per_channel:
+        # for weight, use w_max/quant_max as scale, and convert weight to int8 to save memory.
+        scale = 2 * (data_tensor.abs().max(dim=-1).values.float() / (2**bit_width - 1))
+        quantized_tensor = torch.clamp(torch.round(data_tensor.float() / scale[:, None]), q_min, q_max).to(torch.int8)
+    elif per_token:
+        # per-token quantization
+        scales = 2 * (data_tensor.abs().max(dim=-1).values.float() / (2**bit_width - 1))
+        if len(data_tensor.shape) == 3:
+            scales = scales[:, :, None]
+        elif len(data_tensor.shape) == 2:
+            scales = scales[:, None]
+        else:
+            assert False
+        quantized_tensor = torch.clamp(torch.round(data_tensor.float() / scales.float()), q_min, q_max).to(torch.int8)
+        scale = scales
+    else:
+        # per_tensor quantization
+        scale = data_tensor.abs().max().float() / q_max
+        quantized_tensor = torch.clamp(torch.round(data_tensor.float() / scale.float()), q_min, q_max).to(torch.int8)
+
+    return scale, quantized_tensor
+
+
+class QLinear(nn.Module):
+    def __init__(self, fc: nn.Linear, quant_mode: str):
+        super().__init__()
+        assert quant_mode in ("weight4", "weight8", "dynamic")
+        if quant_mode == 'weight4':
+            weight_bit_width = 4
+        else:
+            weight_bit_width = 8
+
+        self.weight_bit_width = weight_bit_width
+        self.quant_mod = quant_mode
+        self.in_features = fc.in_features
+        self.out_features = fc.out_features
+
+        bias = None if fc.bias is None else fc.bias.data
+        # compress weight by given bit, use per-channel and [-127,127]/[-7,7] to clamp
+        scale, weight_q = compress_int(fc.weight.data, weight_bit_width)
+        if self.weight_bit_width == 4:
+            weight_shape = weight_q.shape
+            assert len(weight_shape) == 2
+            assert weight_shape[1] % 2 == 0
+            pre_packed = weight_q.view(weight_shape[0], weight_shape[1] // 2, 2)
+            weight_q = ((pre_packed[..., 0] & 0b00001111) << 4) | (pre_packed[..., 1] & 0b00001111)
+
+        self.weight = nn.Parameter(weight_q, requires_grad=False)
+        self.weight_scale = nn.Parameter(scale, requires_grad=False)
+        self.bias = nn.Parameter(bias, requires_grad=False) if bias is not None else None
+
+        fc.weight = None
+        fc.bias = None
+
+    def forward(self, input: Tensor) -> Tensor:
+        input_device = input.device
+        input_dtype = input.dtype
+        input_shape = input.shape
+        if self.quant_mod == 'static':
+            assert False, f'{self.quant_mod} not supported'
+        else:
+            if self.quant_mod == 'weight4':
+                if SPEEDUP:
+                    weight_fp = torch.empty(
+                        (self.out_features, self.in_features), dtype=torch.float16, device=input.device
+                    )
+                    decompress_int4(weight_fp, self.weight, self.weight_scale)
+                else:
+                    weight_fp = (
+                        torch.stack((self.weight >> 4, self.weight << 4 >> 4), -1)
+                        .view(self.in_features, -1)
+                        .to(dtype=torch.float32)
+                        * self.weight_scale[:, None]
+                    ).to(dtype=torch.half)
+            elif not SPEEDUP:
+                weight_fp = (self.weight.to(dtype=torch.float32) * self.weight_scale[:, None]).to(dtype=torch.half)
+            elif 'dynamic' not in self.quant_mod:
+                weight_fp = torch.empty_like(self.weight.data, dtype=input_dtype, device=input_device)
+                decompress_int8(weight_fp, self.weight, self.weight_scale)
+
+            if 'dynamic' in self.quant_mod:
+                if SPEEDUP:
+                    # the real dynamic quantization process, first quantize input to int8, then do int8Gemm calculation,
+                    # and finally dequantize the output to float
+                    input_viewed = input.view(-1, input_shape[-1])
+
+                    # init easyquant kernels' output
+                    input_q = torch.empty_like(input_viewed, dtype=torch.int8, device=input_device)
+                    scale_shape = input_viewed.shape[0] if 'token' in self.quant_mod else 1
+                    input_scale = torch.zeros(scale_shape, device=input_device)
+                    out_q = torch.empty(
+                        (int(input_viewed.shape[0]), self.out_features), dtype=torch.int32, device=input_device
+                    )
+                    output = torch.empty_like(out_q, dtype=torch.float16, device=input_device)
+
+                    # use easyquant kernels to accelerate computation
+                    quantize_per_token(input_q, input_viewed, input_scale)
+                    gemm(out_q, input_q, self.weight)
+
+                    if self.bias is not None:
+                        dequantize_bias_per_token(output, out_q, input_scale, self.weight_scale, self.bias)
+                    else:
+                        dequantize_per_token(output, out_q, input_scale, self.weight_scale)
+
+                    output = output.view(input_shape[:-1] + (output.shape[-1],))
+                else:
+                    # simulate quantization
+                    input_scale, input_q = compress_int(
+                        input, 8, per_channel=False, per_token=('token' in self.quant_mod)
+                    )
+                    input_fq = (input_q * input_scale).to(input.dtype).to(input.device)
+                    output = F.linear(input_fq, weight_fp, self.bias)
+            else:
+                input_fq = input
+                output = F.linear(input_fq, weight_fp, self.bias)
+
+        return output
+
+
+class TDQLinear_noinit(QLinear):
+    def forward(self, input: Tensor) -> Tensor:
+        input_shape = input.shape
+        bs, seq, _ = input_shape
+        input_device = input.device
+        input_viewed = input.view(-1, self.in_features)
+
+        input_q = torch.empty_like(input_viewed, dtype=torch.int8, device=input_device)
+        input_scale = torch.empty(bs * seq, device=input_device)
+        out_q = torch.empty((bs * seq, self.out_features), dtype=torch.int32, device=input_device)
+        output = torch.empty_like(out_q, dtype=torch.float16, device=input_device)
+
+        quantize_per_token(input_q, input_viewed, input_scale)
+        gemm(out_q, input_q, self.weight)
+        dequantize_per_token(output, out_q, input_scale, self.weight_scale)
+
+        output = output.view(input_shape[:-1] + (output.shape[-1],))
+        return output
+
+
+@torch.no_grad()
+def fuse_atten(model: nn.Module):
+    """fuse qkv linear, fuse scaled_dot_product_attention if torch>=1.13"""
+    for name, mod in model.named_modules():
+        if isinstance(mod, LlamaAttention):
+            _, parent_mod, last_name = get_submodule_with_parent_from_name(model, name)
+            fused_attn = LlamaAttentionFused(mod)
+            setattr(parent_mod, last_name, fused_attn)
+
+
+@torch.no_grad()
+def quant_fc(model: nn.Module, quant_mod='weight8', fuse_qkv=False):
+    """convert all fcs of LLM model to quantized linear inplace.
+
+    Args:
+        model: the Given LLM model.
+        quant_mod: the working quantization mode. Default to be 'weight8', Optional:['weight4', 'dynamic_token'].
+            The 'dynamic_token' quantization use easyquant lib to do Int8Gemm accelerate.
+        fuse_qkv: whether to fuse qkv linear of attention to speedup inference,
+            the scaled-dot-product-attention will be fusedif the PyTorch version >= 1.13.
+    """
+    model.cpu()
+    log.info(f'use quant mod {quant_mod} speedup={SPEEDUP}')
+    if fuse_qkv:
+        fuse_atten(model)
+        log.info('qkv has been fused')
+
+    for name, mod in model.named_modules():
+        if 'lm_head' in name:
+            continue
+        if isinstance(mod, nn.Linear):
+            _, parent_mod, last_name = get_submodule_with_parent_from_name(model, name)
+            if quant_mod == 'dynamic' and SPEEDUP:
+                quantized_fc_cls = TDQLinear_noinit
+            else:
+                quantized_fc_cls = QLinear
+            quantized_fc = quantized_fc_cls(
+                mod,
+                quant_mod,
+            )
+            setattr(parent_mod, last_name, quantized_fc)
