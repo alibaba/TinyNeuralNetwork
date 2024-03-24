db8331cd99d0bbfaa0c2b6da119e912bbaa53bc9
peterjc123 <peterghost86@gmail.com>
2024-03-24 03:30:26 +0000

    debug

--- tinynn/converter/base.py
+++ tinynn/converter/base.py
@@ -313,7 +313,7 @@ class TFLiteConverter(object):
         self.graph = graph
 
         log.debug('Lowered graph:')
-        log.debug(self.graph)
+        log.warning(self.graph)
 
     def init_flatten_inputs(self):
         self.flatten_inputs.clear()
