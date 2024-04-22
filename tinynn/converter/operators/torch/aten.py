37bdf6c960ce2de8f7f298957b51c7098f550872
peterjc123 <peterghost86@gmail.com>
2024-04-22 16:33:54 +0800

    [quantizer] fix naming for conv-bn1d fusion passes (#295)
    * [quantizer] fix naming for conv-bn fusion passes
    
    * fix lint

--- tinynn/converter/operators/torch/aten.py
+++ tinynn/converter/operators/torch/aten.py
@@ -3404,7 +3404,7 @@ class ATenGeluOperator(ATenGeluSchema):
 
         input_tensor = self.find_or_create_input(0, graph_converter)
         output_tensor = self.to_tfl_tensors(self.output_names, self.output_tensors)[0]
-        
+
         approximate = "none"
         if 'approximate' in args:
             approximate = self.input_tensors[args['approximate']] or "none"
